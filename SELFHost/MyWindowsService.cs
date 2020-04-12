858bf7bbd812792d1e7b6257cf53fa254d35ee65
vihaanaryan <56484843+vihaanaryan@users.noreply.github.com>
2020-04-12 13:31:46 +0530

    adde comment to start

---
+++ SELFHost/MyWindowsService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace SELFHost
+{
+    class MyWindowsService
+    {
+        private static readonly ILog _log = LogManager.GetLogger(nameof(MyWindowsService));
+
+        public void Start()
+        {
+            _log.Info("Started succesfully.");
+        }
+
+        public void Stop()
+        {
+            _log.Info("Stopped succesfully.");
+        }
+    }
+}
---
+++ SELFHost/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SELFHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hostConfig =>
+            {
+
+                hostConfig.UseLog4Net();
+
+                hostConfig.EnableServiceRecovery(serviceRecovery =>
+                {
+                    // first failure, 5 minute delay
+                    serviceRecovery.RestartService(5);
+
+                    // second failure, 10 minute delay
+                    serviceRecovery.RunProgram(10, @"C:\Windows\Notepad.exe");
+
+                    // subsequent failures, 15 minute delay
+                    serviceRecovery.RestartComputer(15, "Topshelf demo failure");
+                });
+                hostConfig.Service<MyWindowsService>(serviceConfig =>
+                {
+                    serviceConfig.ConstructUsing(() => new MyWindowsService());
+                    serviceConfig.WhenStarted(s => s.Start());
+                    serviceConfig.WhenStopped(s => s.Stop());
+                });
+
+              
+
+
+            });
+
+           
+        }
+    }
+}
